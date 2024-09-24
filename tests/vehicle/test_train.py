de7c1373152a13f82fa6f2ed318e47f2d4361d04
Jing Li <jingli@rrc.ca>
2024-09-24 12:40:29 -0500

    completed the train and tests

--- tests/vehicle/test_train.py
+++ tests/vehicle/test_train.py
@@ -8,3 +8,41 @@ the following command:
 """
 
 import unittest
+
+from vehicle.train import Train
+
+class Test(unittest.TestCase):
+    
+    def setUp(self):
+        self.train = Train("Siemens", "Intercity Subway", 13, 0.03)
+
+    def test_init_valid(self):
+        self.assertEqual("Siemens", self.train._Vehicle__make)
+        self.assertEqual("Intercity Subway", self.train._Vehicle__model)
+        self.assertEqual(13, self.train._Train__cars)
+        self.assertEqual(0.03, self.train._Train__base_fuel_rate)
+
+    def test_init_make_invalid_raises_value_error(self):
+        with self.assertRaises(ValueError):
+            train = Train("  ", "Intercity Subway", 13, 0.03)
+    
+    def test_init_model_invalid_raises_value_error(self):
+        with self.assertRaises(ValueError):
+            train = Train("Siemens", "  ", 13, 0.03)
+
+    def test_init_cars_invalid_raises_value_error(self):
+        with self.assertRaises(ValueError):
+            train = Train("Siemens", "Intercity Subway", "cars", 0.03)
+
+    def test_init_base_fuel_rate_invalid_raises_value_error(self):
+        with self.assertRaises(ValueError):
+            train = Train("Siemens", "Intercity Subway", 13, None)
+
+    def test_str(self):
+        expected = 'Make: Siemens \n Model: Intercity Subway\nThis train has a base fuel rate of 0.03 litres/kilometer, and contains 13 cars.'
+    
+    def test_calculate_fuel_requiremen_returns_correct_value(self):
+        self.assertEqual(42.9, self.train.calculate_fuel_requirement(100.0))
+
+
+
--- vehicle/train.py
+++ vehicle/train.py
@@ -1,5 +1,68 @@
 """"
 Description: A class to represent Train objects.
-Author: {Student Name}
-Date: {Date}
+Author: {Jing Li}
+Date: {9/24/2024}
 """
+
+from vehicle.vehicle import Vehicle
+
+
+class Train(Vehicle):
+    """
+    A class representing an train, inheriting from Vehicle.
+
+    Attributes:
+        make(str): Represents the make (manufacturer) of the Train.
+        model(str): Represents the model (specific vehicle model) of the Train.
+        cars(int): Represents the number of cars on the Train.
+        base_fuel_rate(float): Represents the rate at which fuel is burned by the Train.
+    """
+    def __init__(self, make: str, model: str, cars:int, base_fuel_rate:float):
+        """
+        Initialiazes an Train with make, model, base_fuel_rate and cars.
+
+        Args:
+            make (str): The make (manufacturer) of the Train.
+            model (str): The model (specific vehicle model) of the Train.
+            cars (int): The number of cars on the Train.
+            base_fuel_rate (float): The rate at which fuel is burned by the Train.
+
+        Raises:
+            ValueError: If the cars argument is not an int.
+            ValueError: If the base_fuel_rate argument is not a float.
+        """
+        super().__init__(make, model)
+
+        if not isinstance(cars, int):
+            raise ValueError("Cars must be numeric.")
+        else:
+            self.__cars = cars
+
+        if not isinstance(base_fuel_rate,float):
+            raise ValueError("Base fuel rate must be numeric")
+        else:
+            self.__base_fuel_rate = base_fuel_rate
+
+    def __str__(self) -> str:
+        """
+        Returns a string representation of the train.
+
+        Returns:
+            str: A string describing the train with the given arguments.
+
+        """
+        return super().__str__()+f'\nThis train has a base fuel rate of {self.__base_fuel_rate} litres/kilometer, and contains {self.__cars} cars.'
+
+    def calculate_fuel_requirement(self, distance: float) -> float:
+        """
+        Calculate the amount of fuel needed by the train for the given distance.
+
+        Args:
+            distance (float): the distance to travel in kilometers.
+
+        Returns:
+            float: the calculated fuel requirements for the given distance.
+        """
+        total_fuel = self.__base_fuel_rate * (1.1 * self.__cars)
+        fuel_requirements = total_fuel * distance
+        return fuel_requirements
